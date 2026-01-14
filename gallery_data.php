952aece1b7e8162f6a1d8a841ca8102ecc7e74c8
Azhar-A1 <abiblora2019@gmail.com>
2026-01-14 23:11:36 +0700

    gallery_data.php

--- gallery_data.php
+++ gallery_data.php
@@ -44,41 +44,42 @@
                                 <a href="#" title="delete" class="badge rounded-pill text-bg-danger" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $row["id"] ?>"><i class="bi bi-x-circle"></i></a>
                                 
                                 <!-- Awal Modal Edit -->
-                                <div class="modal fade" id="modalEdit<?= $row["id"] ?>" tabindex="-1">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Edit Gallery</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
+                                <div class="modal fade" id="modalEdit<?= $row["id"] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Edit Gallery</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <form method="post" action="" enctype="multipart/form-data">
+                                                <div class="modal-body">
+                                                    <div class="mb-3">
+                                                        <label for="formGroupExampleInput2" class="form-label">Ganti Gambar</label>
+                                                        <input type="file" class="form-control" name="gambar">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="formGroupExampleInput3" class="form-label">Gambar Lama</label>
+                                                        <?php
+                                                        if ($row["gambar"] != '') {
+                                                            if (file_exists('img/' . $row["gambar"])) {
+                                                        ?>
+                                                            <br><img src="img/<?= $row["gambar"] ?>" width="100">
+                                                        <?php
+                                                            }
+                                                        }
+                                                        ?>
 
-            <form method="post" action="admin.php?page=gallery" enctype="multipart/form-data">
-                <input type="hidden" name="id" value="<?= $row["id"] ?>">
-                <input type="hidden" name="gambar_lama" value="<?= $row["gambar"] ?>">
-
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label class="form-label">Ganti Gambar</label>
-                        <input type="file" class="form-control" name="gambar">
-                    </div>
-
-                    <div class="mb-3">
-                        <label class="form-label">Gambar Lama</label><br>
-                        <?php if ($row["gambar"] && file_exists("img/".$row["gambar"])) { ?>
-                            <img src="img/<?= $row["gambar"] ?>" width="120">
-                        <?php } ?>
-                    </div>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                    <button type="submit" name="edit_gambar" class="btn btn-primary">Simpan</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
+                                                        <input type="hidden" name="gambar_lama" value="<?= $row["gambar"] ?>">
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                    <input type="submit" value="simpan" name="simpan" class="btn btn-primary">
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
                                 <!-- Akhir Modal Edit -->
 
                                 <!-- Awal Modal Hapus -->
@@ -89,7 +90,7 @@
                                                 <h1 class="modal-title fs-5" id="staticBackdropLabel">Konfirmasi Hapus Foto</h1>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
-                                            <input type="hidden" name="id" value="<?= $row["id"] ?>">
+                                            <form method="post" action="" enctype="multipart/form-data">
                                                 <div class="modal-body">
                                                     <div class="mb-3">
                                                         <label for="formGroupExampleInput" class="form-label">Yakin akan menghapus foto?</label>
